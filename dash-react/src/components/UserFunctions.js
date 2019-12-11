ac288df651c02e91c8dee601fc15e3842a09d65a
Alex <ajbarbati@gmail.com>
2019-12-11 15:37:05 -0500

    forgot to push

---
+++ dash-react/src/components/UserFunctions.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export const register = newUser => {
+    return axios
+    .post('users/register', {
+        first_name: newUser.first_name,
+        last_name: newUser.last_name,
+        email: newUser.email,
+        password: newUser.password
+    })
+}
+
+export const login = user => {
+    return axios
+    .post('users/login', {
+        email: user.email,
+        password: user.password
+    })
+    .then(res => {
+        localStorage.setItem('usertoken', res.data)
+        return res.data
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
